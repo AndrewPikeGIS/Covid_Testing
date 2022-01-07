edc0c9735b75a02b4b0b93e622822ef78dc927d0
Andrew Pike <andrewpikegis@gmail.com>
2022-01-06 23:10:45 -0700

    updated sask data location

--- R/rsconnect_functions.r
+++ R/rsconnect_functions.r
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-#run this
-rsconnect::setAccountInfo(name='andrew-pike-sds', token='4549BB557E8948BBEE60C3CD75EC3698', secret='UXxMZJgy2EocShHeyw8WakrLvuvlqvLEpR4O57Bm')
-=======
 #rsconnect and push app
 #library(rsconnect)
 
@@ -13,4 +9,3 @@ rsconnect::setAccountInfo(name='andrew-pike-sds', token='4549BB557E8948BBEE60C3C
 
 
 #rsconnect::deployApp("C:/Users/pia24919/andrew/COVID_Proj")
->>>>>>> 07245259f470389c03a158d5d8a2c27db24ba4df
