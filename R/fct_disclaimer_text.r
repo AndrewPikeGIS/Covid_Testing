35173ac12e89fdb2fe894d053863e9899e3eb526
Andrew Pike <andrewpikegis@gmail.com>
2022-01-17 21:59:49 -0700

    fixed the disclaimer text

--- R/fct_disclaimer_text.r
+++ R/fct_disclaimer_text.r
@@ -1,11 +1,13 @@
 build_disclaimer <- function() {
     disclaimer <- paste0(
         "This app is meant as a small showcase of my Shiny dashboard ",
-        "development and not an official dashboard for case tracking. ",
+        "development and not an official dashboard for case tracking.\n\n",
         "Due to local government cutbacks, PCR testing has been scaled ",
-        "back across many of the regions where data is pulled from. ",
+        "back across many of the regions where data is pulled from.\n",
         "As a resuly, case numbers should be assumed to be higher than ",
-        "reported."
+        "reported.\n\n",
+        "Please see my github for the app repository: \n",
+        "https://github.com/AndrewPikeGIS/Covid_Testing"
     )
 
     return(disclaimer)
--- R/mod_render_text.r
+++ R/mod_render_text.r
@@ -1,6 +1,6 @@
 mod_render_text_ui <- function(id) {
     ns <- NS(id)
-    textOutput(ns("disclaimer_text"))
+    verbatimTextOutput(ns("disclaimer_text"))
 }
 
 mod_render_text_server <- function(id) {
